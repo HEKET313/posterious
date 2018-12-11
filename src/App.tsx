f8f0599293a662faed400a01be4aa4d65aa2ab51
Nikita Leshchev <nleshchev@ozon.travel>
2018-12-11 23:52:50 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './App.css';
+import LoginWrapper from "./components/LoginWrapper/LoginWrapper";
+
+class App extends React.Component {
+  public render() {
+    return (
+        <LoginWrapper/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/LoginWrapper/LoginWrapper.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+class LoginWrapper extends React.Component {
+    render() {
+        return <div className="wrapper">
+
+        </div>;
+    }
+}
+
+export default LoginWrapper;
