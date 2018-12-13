4c81b25de73167903cf820a994476941020fe77c
Nikita Leshchev <nleshchev@ozon.travel>
2018-12-13 22:07:39 +0300

    Registration form + ready login form

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,41 +1,102 @@
 import * as React from "react";
-import {Button, Form, FormGroup, Input, Label} from "reactstrap";
+import {Button, Form, FormFeedback, FormGroup, Input, Label} from "reactstrap";
+import * as EmailValidator from "email-validator";
 
-class LoginForm extends React.Component<{},{}>
-{
-    render(): React.ReactNode {
+export interface LoginFormProps {
+    login?: string;
+    pass?: string;
+}
+
+interface LoginState {
+    globalError: string;
+    passwordError: string;
+    loginError: string;
+    login: string;
+    pass: string;
+}
+
+class LoginForm extends React.Component<LoginFormProps, LoginState> {
+    constructor(props: LoginFormProps) {
+        super(props);
+        this.state = {
+            globalError: "",
+            passwordError: "",
+            loginError: "",
+            login: props.login || "",
+            pass: props.pass || ""
+        };
+    }
+
+    public render(): React.ReactNode {
+        let {passwordError, loginError, globalError} = this.state;
+        let isPassInvalid = passwordError.length > 0 || globalError.length > 0;
+        let isLoginInvalid = loginError.length > 0 || globalError.length > 0;
+        let passwordErrorMessage = isPassInvalid ? <FormFeedback>{passwordError + globalError}</FormFeedback> : null;
+        let loginErrorMessage = loginError.length > 0 ? <FormFeedback>{this.state.loginError}</FormFeedback> : null;
         return <Form>
             <FormGroup>
                 <Label for="login">Email</Label>
-                <Input type="email" name="login" id="login" placeholder="Login" />
+                <Input type="email" name="login" id="login" placeholder="Login" invalid={isLoginInvalid}
+                       onChange={this.loginChange}/>
+                {loginErrorMessage}
             </FormGroup>
             <FormGroup>
-                <Label for="password">Password</Label>
-                <Input type="password" name="password" id="password" placeholder="******" />
+                <Label for="password">Пароль</Label>
+                <Input type="password" name="password" id="password" placeholder="******" invalid={isPassInvalid}
+                       onChange={this.passChange}/>
+                {passwordErrorMessage}
             </FormGroup>
             <Button color="primary" onClick={this.login}>Login</Button>
         </Form>;
     }
 
-    private async login(): Promise<boolean> {
+    private loginChange = (e: any) => this.setState({login: e.target.value});
+    private passChange = (e: any) => this.setState({pass: e.target.value});
+
+    private login = async (): Promise<boolean> => {
         try {
-            const response = await fetch('some-url', {
+            this.clearErrors();
+            if (!this.checkInput()) {
+                return false;
+            }
+            const response = await fetch('http://demo9936313.mockable.io/login', {
                 method: 'post',
                 headers: new Headers({
                     "Content-Type": "application/json",
                     Accept: "application/json"
                 }),
-                body: JSON.stringify(this.state)
+                body: JSON.stringify({
+                    login: this.state.login,
+                    pass: this.state.pass
+                })
             });
             if (response.status !== 200) {
-                console.log(response);
+                this.setState({globalError: 'Такой комбинации не существует'});
                 return false;
             }
-            return response.ok;
+            return true;
         } catch (e) {
+            console.error(e);
             return false;
         }
-    }
+    };
+
+    private checkInput = (): boolean => {
+        let result: boolean = true;
+        if (!EmailValidator.validate(this.state.login)) {
+            this.setState({loginError: "Логин должен быть валидным email'ом"});
+            result = false;
+        }
+        if (this.state.pass.length === 0) {
+            this.setState({passwordError: "Введите пароль"});
+            result = false;
+        }
+        return result;
+    };
+
+    private clearErrors = (): void => {
+        this.setState({globalError: "", loginError: "", passwordError: ""})
+    };
 }
 
 export default LoginForm;
---
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,97 @@
+import * as React from "react";
+import {Button, Form, FormFeedback, FormGroup, Input, Label} from "reactstrap";
+import * as EmailValidator from "email-validator";
+
+interface State {
+    login: string;
+    pass: string;
+    rePass: string;
+    loginError: string;
+    passwordError: string;
+    globalError: string;
+}
+
+class RegistrationForm extends React.Component<object, State> {
+
+    render(): React.ReactNode {
+        const {loginError, passwordError, globalError} = this.state;
+        const isLoginInvalid = loginError.length > 0 || globalError.length > 0;
+        const isPassPairInvalid = passwordError.length > 0 || globalError.length > 0;
+        const loginErrorMessage = loginError.length > 0 ? <FormFeedback>{loginError}</FormFeedback> : null;
+        const passwordErrorMessage = isPassPairInvalid
+            ? <FormFeedback>{passwordError + globalError}</FormFeedback>
+            : null;
+        return <Form>
+            <FormGroup>
+                <Label for="login">Email</Label>
+                <Input type="email" name="login" id="login" placeholder="Login" invalid={isLoginInvalid}
+                       onChange={this.loginChange}/>
+                {loginErrorMessage}
+            </FormGroup>
+            <FormGroup>
+                <Label for="password">Пароль</Label>
+                <Input type="password" name="password" id="password" placeholder="******" invalid={isPassPairInvalid}
+                       onChange={this.passChange}/>
+            </FormGroup>
+            <FormGroup>
+                <Label for="password">Повторите пароль</Label>
+                <Input type="password" name="re-password" id="re-password" placeholder="******"
+                       invalid={isPassPairInvalid} onChange={this.rePassChange}/>
+                {passwordErrorMessage}
+            </FormGroup>
+            <Button color="primary" onClick={this.register}>Зарегистрироваться</Button>
+        </Form>;
+    }
+
+    private loginChange = (e: any) => this.setState({login: e.target.value});
+    private passChange = (e: any) => this.setState({pass: e.target.value});
+    private rePassChange = (e: any) => this.setState({rePass: e.target.value});
+
+    private register = async (): Promise<boolean> => {
+        if (!this.checkInput()) {
+            return false;
+        }
+        try {
+            const response = await fetch('http://demo9936313.mockable.io/register', {
+                method: 'post',
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                }),
+                body: JSON.stringify({
+                    login: this.state.login,
+                    pass: this.state.pass,
+                    rePass: this.state.rePass
+                })
+            });
+            if (response.status !== 200) {
+                this.setState({globalError: 'Ошибка регистрации'});
+                return false;
+            }
+            return true;
+        } catch (e) {
+            console.error(e);
+            this.setState({globalError: e.message});
+            return false;
+        }
+    };
+
+    private checkInput = (): boolean => {
+        let result: boolean = true;
+        if (!EmailValidator.validate(this.state.login)) {
+            this.setState({loginError: "Логин должен быть валидным email'ом"});
+            result = false;
+        }
+        if (this.state.pass.length < 6) {
+            this.setState({passwordError: "Пароль должен быть не менее 6 символов"});
+            result = false;
+        }
+        if (this.state.pass !== this.state.rePass) {
+            this.setState({passwordError: "Пароли должны быть идентичны"});
+            result = false;
+        }
+        return result;
+    };
+}
+
+export default RegistrationForm;
